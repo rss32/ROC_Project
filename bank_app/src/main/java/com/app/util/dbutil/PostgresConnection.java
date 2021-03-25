9470557924c44d7a35f605f4630fe4961c9e2d51
rss32 <write_me_later@hotmail.com>
2021-03-25 09:01:08 -0400

    Removed comments

--- bank_app/src/main/java/com/app/util/dbutil/PostgresConnection.java
+++ bank_app/src/main/java/com/app/util/dbutil/PostgresConnection.java
@@ -13,14 +13,12 @@ public class PostgresConnection {
 	public static Connection getConnection()
 			throws ClassNotFoundException, SQLException {
 
-		// if (connection == null) {
 		Class.forName("org.postgresql.Driver");
 		String url = "jdbc:postgresql://localhost:5432/postgres";
 		String username = "postgres";
 		String password = "revature";
 		connection = DriverManager.getConnection(url, username, password);
-		// }
+		
 		return connection;
-
 	}
 }
